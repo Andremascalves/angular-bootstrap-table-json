27a1d83c56b8805225e0061087208b620818940f
andremascalves <andremascalves@gmail.com>
2023-11-28 22:42:39 -0300

    Tabela montada pelo Json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
 
+import OrdersJson from './orders.json';
+
+
+interface ORDERS {
+
+  "id": Number;
+  "customer": String;
+  "address1": String;
+  "city": String;
+  "date": String;
+
+  }
+
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'angular-bootstrap-table-json';
+ 
+  title = `aa`;
+
+
+  Orders: ORDERS[] = OrdersJson;
+ 
+  constructor(){
+    console.log(this.Orders);
+  }
+  
 }
